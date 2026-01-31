eaf2ec4afcc0273bb9d15d5eb8f1d5d89c98ea45
Saidel Lopez <sailopher@yahoo.com.mx>
2026-01-30 23:49:29 -0600

    finished the two last challenges

--- KotlinGoogleTraining/src/main/java/com/saishaddai/FoldablePhones.kt
+++ KotlinGoogleTraining/src/main/java/com/saishaddai/FoldablePhones.kt
@@ -1,2 +1,56 @@
 package com.saishaddai
 
+/**
+ * Typically, a phone screen turns on and off when the power button is pressed. In contrast, if a foldable phone is folded, the main inner screen on a foldable phone doesn't turn on when the power button is pressed.
+ *
+ * In the initial code provided in the following code snippet, write a FoldablePhone class that inherits from the Phone class. It should contain the following:
+ *
+ * A property that indicates whether the phone is folded.
+ * A different switchOn() function behavior than the Phone class so that it only turns the screen on when the phone isn't folded.
+ * Methods to change the folding state.
+ */
+
+open class Phone(var isScreenLightOn: Boolean = false) {
+    open fun switchOn() {
+        isScreenLightOn = true
+    }
+
+    fun switchOff() {
+        isScreenLightOn = false
+    }
+
+    fun checkPhoneScreenLight() {
+        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+        println("The phone screen's light is $phoneScreenLight.")
+    }
+}
+
+class FoldablePhone(var isFolded : Boolean = false) : Phone() {
+    fun fold() {
+        isFolded = true
+    }
+
+    fun unfold() {
+        isFolded = false
+    }
+
+    override fun switchOn() {
+        if(!isFolded) {
+            isScreenLightOn = true
+        }
+    }
+
+}
+
+fun main() {
+    val newFoldablePhone = FoldablePhone()
+    newFoldablePhone.checkPhoneScreenLight()
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
+    newFoldablePhone.unfold()
+    newFoldablePhone.checkPhoneScreenLight()
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
+    newFoldablePhone.switchOff()
+    newFoldablePhone.checkPhoneScreenLight()
+}
